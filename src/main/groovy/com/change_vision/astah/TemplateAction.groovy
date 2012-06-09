d24aa2a0f2c77d5751af97d0ed69e12723a6c318
Hiroki Kondo <kompiro@gmail.com>
2012-06-09 23:34:39 +0900

    make java source code to be groovy

--- src/main/groovy/com/change_vision/astah/TemplateAction.groovy
+++ src/main/groovy/com/change_vision/astah/TemplateAction.groovy
@@ -10,21 +10,20 @@ import com.change_vision.jude.api.inf.ui.IPluginActionDelegate;
 import com.change_vision.jude.api.inf.ui.IPluginActionDelegate.UnExpectedException;
 import com.change_vision.jude.api.inf.ui.IWindow;
 
-public class TemplateAction implements IPluginActionDelegate {
+class TemplateAction implements IPluginActionDelegate {
 
-  public Object run(IWindow window) throws UnExpectedException  {
+  def run(IWindow window) {
       try {
-          ProjectAccessor projectAccessor = ProjectAccessorFactory.getProjectAccessor();
-          projectAccessor.getProject();
-          JOptionPane.showMessageDialog(window.getParent(),"Hello");
+        def projectAccessor = ProjectAccessorFactory.getProjectAccessor();
+        projectAccessor.getProject();
+        JOptionPane.showMessageDialog(window.getParent(),"Hello");
       } catch (ProjectNotFoundException e) {
-          String message = "Project is not opened.Please open the project or create new project.";
-      JOptionPane.showMessageDialog(window.getParent(), message, "Warning", JOptionPane.WARNING_MESSAGE);
+        def message = "Project is not opened.Please open the project or create new project.";
+        JOptionPane.showMessageDialog(window.getParent(), message, "Warning", JOptionPane.WARNING_MESSAGE);
       } catch (Exception e) {
         JOptionPane.showMessageDialog(window.getParent(), "Unexpected error has occurred.", "Alert", JOptionPane.ERROR_MESSAGE);
         throw new UnExpectedException();
       }
-      return null;
   }
 
 
